3e70e568657039a9f542c02b0a52394f752fb1a3
ziaa404 <zev8065@gmail.com>
2026-06-22 17:11:33 +0300

    Add Procfile and PORT config for deployment

--- app.py
+++ app.py
@@ -239,4 +239,5 @@ def api_report(client_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
